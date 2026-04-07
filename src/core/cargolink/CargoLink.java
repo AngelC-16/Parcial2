e2666f1430bb11267b3e2452619de8e200597249
DG-AMIR3460 <amird@uninorte.edu.co>
2026-04-07 18:23:08 -0500

    Change

--- src/core/cargolink/CargoLink.java
+++ src/core/cargolink/CargoLink.java
@@ -4,10 +4,42 @@
  */
 package core.cargolink;
 
-/**
- *
- * @author acarrillog
- */
+import core.vehicle.Plane;
+import core.vehicle.Ship;
+import core.vehicle.Truck;
+import core.vehicle.Vehicle;
+import core.client.Client;
+import core.cargolink.CargoLink;
+import core.cargolink.logistics.LogisticsCenter;
+import core.cargolink.logistics.Shipping.Shipping;
+import java.util.ArrayList;
+
+
 public class CargoLink {
+    private ArrayList<Client> client;
+    private ArrayList<Center> center;
+    private ArrayList<Vehicle> vehicles;
+    private final ArrayList<Object> vehicle;
+    
+    public CargoLink(){
+        this.client = new ArrayList<>();
+        this.center = new ArrayList<>();
+        this.vehicle = new ArrayList<>();
+    }    
+    
+    public ArrayList<Client> getClients() {
+        return client;
+    }
+
+    public ArrayList<Center> getCenter() {
+        return center;
+    }
+
+    public ArrayList<Vehicle> getVehicles() {
+        return vehicles;
+    }
+    
+    
+    
     
 }
